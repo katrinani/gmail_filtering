a7df8fa5bffec9b78d43140f80d831f16b601b27
katrina <anisimovakaterina328@gmail.com>
2024-10-05 22:40:40 +0500

    Add: Функция для удаления ненужных сообщений

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from simplegmail import Gmail
+from simplegmail.label import Label
 from simplegmail.message import Message
 
 gmail = Gmail()
@@ -19,6 +20,17 @@ def filtering_no_reply(sender: str) -> list[Message]:
     return list_of_message_with_noreply
 
 
+def hanging_message_labels(list_of_message: list[Message], name_of_label: str) -> None:
+    labels = gmail.list_labels()  # все метки
+
+    label: list[Label] = list(filter(lambda x: x.name == name_of_label.upper(), labels))
+    if label is not []:
+        for message in list_of_message: message.add_label(label[0])
+    else:
+        print("Не найдена метка")
+
+
 if __name__ == '__main__':
     text_from = '"Не нужно отвечать на это сообщение (отправлено через eu.iit.csu.ru)" <noreply@eu.iit.csu.ru>'
-    filtering_no_reply(text_from)
+    mes = filtering_no_reply(text_from)
+    hanging_message_labels(mes, "TRASH")
