54b83b60b8f5c521b328f9f1078b6da0dbd568b7
katrina <anisimovakaterina328@gmail.com>
2024-10-05 17:18:04 +0500

    Add: Bibl test

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from email.message import Message
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+from simplegmail import Gmail
 
 
-# Press the green button in the gutter to run the script.
+def print_hi():
+    gmail = Gmail()
+
+    mess = gmail.get_starred_messages()
+    for mes in mess:
+        print(mes.recipient, mes.sender, mes.subject, mes.date, mes.snippet, mes.plain)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_hi()
