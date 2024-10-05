a1b1d5ec5238acffac3a85465893847998abc044
katrina <anisimovakaterina328@gmail.com>
2024-10-05 18:14:44 +0500

    Add: Функция для фильтрации сообщений

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from email.message import Message
-
 from simplegmail import Gmail
+from simplegmail.message import Message
 
+gmail = Gmail()
 
-def print_hi():
-    gmail = Gmail()
+def filtering_no_reply(sender: str) -> list[Message]:
+    """
+    Фильтрация непрочитанных сообщений по заданному отправителю
+    :param sender: Строка, которая должна содержаться в поле "Отправитель" в сообщении
+    :return: Список отфильтрованных сообщений
+    """
+    list_of_message_with_noreply = []
+    mess: list[Message] = gmail.get_unread_inbox()  # непрочитанные сообщения
 
-    mess = gmail.get_starred_messages()
     for mes in mess:
-        print(mes.recipient, mes.sender, mes.subject, mes.date, mes.snippet, mes.plain)
+        if sender in mes.sender:
+            list_of_message_with_noreply.append(mes)
+
+    return list_of_message_with_noreply
 
 
 if __name__ == '__main__':
-    print_hi()
+    text_from = '"Не нужно отвечать на это сообщение (отправлено через eu.iit.csu.ru)" <noreply@eu.iit.csu.ru>'
+    filtering_no_reply(text_from)
